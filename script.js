e85c950339290b44080c1642a3b7922943ce4d03
SlappingMoth <maverickclements@gmail.com>
2025-10-04 01:52:20 +0100

    working on updating the school

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// Function for opening and closing cards in html. 
+function openCard(card) {
+  document.getElementById('overlay').style.display = 'flex';
+}
 
-
-function goToContacts() {
-  window.location.href = "contactInfo.html";
-}   
-
-
-
-const button = document.getElementById("cta");
-button.addEventListener("click", handleClick);
-
+function closeCard(event) {
+  // Prevent close if clicking inside the card itself
+  if (event.target.classList.contains('card') || event.target.classList.contains('basic') || event.target.classList.contains('details')) {
+    return;
+  }
+  document.getElementById('overlay').style.display = 'none';
+}
 
