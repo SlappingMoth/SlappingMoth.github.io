7f41ebfd67554a3ded0a143eb4266271ec710f12
SlappingMoth <maverickclements@gmail.com>
2025-11-16 06:16:45 +0000

    trying to work with the files

--- games/codebreaker/codebreaker.js
+++ games/codebreaker/codebreaker.js
@@ -1,6 +1,15 @@
 
 // function to check if the guess was correct
-function check_guess (num) {
-    
+
+guess = document.getElementById("guess");
+missed = document.getElementById("missed-list");
+
+
+function validate_guess () {
+
+    if (guess == 10){
+        missed += 1;
+    }
 
 }
+
