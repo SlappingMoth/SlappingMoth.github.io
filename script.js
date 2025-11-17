e70b6ec0945f89ec9b674a88fa5b202ccf88d4a0
SlappingMoth <maverickclements@gmail.com>
2025-11-17 03:52:07 +0000

    adding a return button

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ function closeCard(event) {
 // event listener to add spinning while game loads - for games.html
 document.getElementById("play-btn").addEventListener("click", function () {
       // Show game card, hide play card
-      document.getElementById("play-card").style.display = "none";
-      document.getElementById("game-card").style.display = "block";     
+    document.getElementById("play-card").style.display = "none";
+    document.getElementById("game-card").style.display = "block";     
 });
+
+document.getElementById("end-btn").addEventListener("click", function () {
+    
+    document.getElementById("play-card").style.display = "block";
+    document.getElementById("game-card").style.display = "none";     
+
+    
+})
