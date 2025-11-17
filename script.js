b87ad9b4b782756e3b1b38b2c61e2a13784b4202
SlappingMoth <maverickclements@gmail.com>
2025-11-17 04:27:12 +0000

    changing expansion

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function closeCard(event) {
   document.getElementById('overlay').style.display = 'none';
 }
 
-// event listener to add spinning while game loads - for games.html
-document.getElementById("play-btn").addEventListener("click", function () {
-      // Show game card, hide play card
-    document.getElementById("play-card").style.display = "none";
-    document.getElementById("game-card").style.display = "block";     
-});
+// // event listener to add spinning while game loads - for games.html
+// document.getElementById("play-btn").addEventListener("click", function () {
+//       // Show game card, hide play card
+//     document.getElementById("play-card").style.display = "none";
+//     document.getElementById("game-card").style.display = "block";     
+// });
 
 document.getElementById("end-btn").addEventListener("click", function () {
     
